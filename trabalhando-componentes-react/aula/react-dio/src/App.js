9b826ba6342e5bd0987fb10c6a78796adac5abdb
leonardoklestadtluz <klestadt@hotmail.com>
2021-10-31 01:53:02 -0300

    Modificação realizada nos arquivos index.html e App.js. E inclusão da pasta components

--- trabalhando-componentes-react/aula/react-dio/src/App.js
+++ trabalhando-componentes-react/aula/react-dio/src/App.js
@@ -1,6 +1,15 @@
+import Item from './components/item';
+
 const App = () => {
   return (
-    <h1>Hello World CONTINUA!!!!</h1>
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </ul>
+    </>
   )
 }
 
