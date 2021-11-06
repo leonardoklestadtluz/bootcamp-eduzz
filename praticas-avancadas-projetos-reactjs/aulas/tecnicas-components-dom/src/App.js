24a2e4fd22673308d653a274ebbc8dadc7a25339
leonardoklestadtluz <klestadt@hotmail.com>
2021-11-06 12:47:41 -0300

    Sincronização de repositório local e remoto

--- praticas-avancadas-projetos-reactjs/aulas/tecnicas-components-dom/src/App.js
+++ praticas-avancadas-projetos-reactjs/aulas/tecnicas-components-dom/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+
+const store = [{
+  type: 'Calça'
+}, {
+  type: 'Calçado'
+}, {
+  type: 'Camiseta'
+}]
+
+function Column({ type }) {
+  return (
+    <tr>
+      <td>{type.name.console}</td>
+    </tr>
+  )
+}
 
 function App() {
+
+  const renderColumns = (element, key) => {
+    <Fragment key={`column - ${key}`}>
+      <Column type={element.type} />
+    </Fragment>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <tr>
+        {store.map(renderColumns)}
+      </tr>
+    </table>
   );
 }
 
--- praticas-avancadas-projetos-reactjs/aulas/tecnicas-components-dom/src/index.js
+++ praticas-avancadas-projetos-reactjs/aulas/tecnicas-components-dom/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
+
+const main = (
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  main,
   document.getElementById('root')
 );
