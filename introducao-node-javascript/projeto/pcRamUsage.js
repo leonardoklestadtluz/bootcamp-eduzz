bf488d0b80c795ff5c908ae6648c40fa8e066eb5
leonardoklestadtluz <klestadt@hotmail.com>
2021-11-18 01:40:01 -0300

    Conclusão do curso Introdução ao NodeJS com JavaScript

---
+++ introducao-node-javascript/projeto/pcRamUsage.js
@@ -0,0 +1,20 @@
+const { clear } = require('console');
+const os = require('os');
+
+setInterval(() => {
+    const { arch, platform, totalmem, freemem } = os;
+    const tRam = totalmem() / 1024 / 1024;
+    const fRam = freemem() / 1024 / 1024;
+    const usage = (fRam / tRam) * 100;
+
+    const stats = {
+        OS: platform(),
+        Arch: arch(),
+        TotalRAM: `${parseInt(tRam)} MB`,
+        FreeRAM: `${parseInt(fRam)} MB`,
+        Usage: `${parseInt(usage).toFixed(2)} %`
+    }
+    console.clear();
+    console.table(stats);
+    exports.stats = stats; // exportando o objeto stats
+}, 1000);
